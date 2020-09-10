2de7ddbd60b58eff11186c5dd71c16b04bce6f22
Dieimes Gualberto <dieimesgualberto@gmail.com>
2020-09-09 22:08:30 -0300

    Add home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('base')
+
+@section('title', 'Home')
+
+@section('content')
+<div class="title m-b-md">
+    Home
+</div>
+<div>
+    <a href="/escolas">Escolas</a>
+    <a href="/cursos">Cursos</a>
+    <a href="/series">Series</a>
+    <a href="/turmas">Turmas</a>
+    <a href="/alunos">Alunos</a>
+</div>
+
+@endsection
